05af6e86ec19d88f105974270fa5bb517dd40add
Sanjay Chauhan <sabby3861@gmail.com>
2026-04-21 21:39:55 +0100

    Initial commit: Added empty root tab

--- APODExplorer/APODExplorerApp.swift
+++ APODExplorer/APODExplorerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct APODExplorerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootTabView()
         }
     }
 }
---
+++ APODExplorer/RootTabView.swift
@@ -0,0 +1,18 @@
+//
+//  RootTabView.swift
+//  APODExplorer
+//
+//  Created by Sanjay Kumar on 21/04/2026.
+//
+
+import SwiftUI
+
+struct RootTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootTabView()
+}
